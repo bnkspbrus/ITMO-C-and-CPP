10eee07e5a3a25ceafe10ec2672e404c52e0f9d3
badger_in_a_burrow <80609501+bnkspbrus@users.noreply.github.com>
2021-05-27 22:12:04 +0300

    Add files via upload

---
+++ LN.h
@@ -0,0 +1,60 @@
+﻿#ifndef LN_H
+#define LN_H
+
+#include <string_view>
+
+
+class LN
+{
+public:
+    bool negate;
+    int array_size;
+    int *digit;
+
+    ~LN();
+
+    LN(const LN &other);
+
+    LN(long long number);
+
+    LN(const char *string);
+
+    LN(std::string_view string);
+
+    LN(LN &&other);
+
+    LN(int array_size, int *digit, bool negate);
+
+    LN operator_() const;
+
+    LN &operator=(const LN &other);
+
+    LN operator+(const LN &other) const;
+
+    LN operator-(const LN &other) const;
+
+    LN &operator*(const LN &other) const;
+
+    LN &operator/(const LN &other) const;
+
+    LN &operator%(const LN &other) const;
+
+    LN operator~() const;
+
+    bool operator<(const LN &other) const;
+
+    bool operator<=(const LN &other) const;
+
+    bool operator>(const LN &other) const;
+
+    bool operator>=(const LN &other) const;
+
+    bool operator==(const LN &other) const;
+
+    bool operator!=(const LN &other) const;
+
+    std::string toString() const;
+};
+
+
+#endif //LN_H
---
+++ main.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <stack>
+#include <fstream>
+#include "LN.h"
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    ifstream fin;
+    fin.open("input.txt");
+    ofstream fout;
+    fout.open("output.txt");
+    if (!fin.is_open())
+    {
+
+    }
+    string arr_bin[] = {"+", "-", "*", "/", "%", "<", "<=", ">", ">=", "==", "!="};
+    string arr_un[] = {"_", "~"};
+    set<string> bin_ops(arr_bin, arr_bin + 11);
+    set<string> un_ops(arr_un, arr_un + 2);
+    stack<string> stack;
+    while (!fin.eof())
+    {
+        string str;
+        getline(fin, str);
+        stack.push(str);
+        if (!un_ops.count(stack.top()) && !bin_ops.count(stack.top()))
+        {
+            string arg1 = stack.top();
+            stack.pop();
+            if (!un_ops.count(stack.top()) && !bin_ops.count(stack.top()))
+            {
+                string arg2 = stack.top();
+                stack.pop();
+                if (bin_ops.count(stack.top()))
+                {
+                    string fun = stack.top();
+                    stack.pop();
+                    LN a1(arg1);
+                    LN a2(arg2);
+                    if (fun == "+")
+                        stack.push((a1 + a2).toString());
+                    else if (fun == "-")
+                        stack.push((a1 - a2).toString());
+                    else if (fun == "*")
+                        stack.push((a1 * a2).toString());
+                    else if (fun == "/")
+                        stack.push((a1 / a2).toString());
+                    else if (fun == "%")
+                        stack.push((a1 % a2).toString());
+                    else if (fun == "<")
+                        stack.push(a1 < a2 ? "1" : "0");
+                    else if (fun == "<=")
+                        stack.push(a1 <= a2 ? "1" : "0");
+                    else if (fun == ">")
+                        stack.push(a1 > a2 ? "1" : "0");
+                    else if (fun == ">=")
+                        stack.push(a1 >= a2 ? "1" : "0");
+                    else if (fun == "==")
+                        stack.push(a1 == a2 ? "1" : "0");
+                    else
+                        stack.push(a1 != a2 ? "1" : "0");
+                }
+                else
+                {
+                    //throw
+                }
+            }
+            else
+            {
+                if (un_ops.count(stack.top()))
+                {
+                    string fun = stack.top();
+                    stack.pop();
+                    LN a1(arg1);
+                    if (fun == "_")
+                        stack.push(a1.operator_().toString());
+                    else
+                        stack.push(a1.operator~().toString());
+                }
+                else
+                {
+                    stack.push(arg1);
+                }
+            }
+        }
+    }
+    while (!stack.empty())
+    {
+        fout << stack.top() << endl;
+        stack.pop();
+    }
+    fin.close();
+    fout.close();
+}
+
+
+
